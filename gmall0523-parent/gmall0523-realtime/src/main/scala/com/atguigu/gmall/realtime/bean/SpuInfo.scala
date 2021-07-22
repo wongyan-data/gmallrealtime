b73bb9c84401d61f9e8037b9e3074dac635e267c
Travis-computer <413582198@qq.com>
2021-07-22 10:42:00 +0800

    local to github

---
+++ gmall0523-parent/gmall0523-realtime/src/main/scala/com/atguigu/gmall/realtime/bean/SpuInfo.scala
@@ -0,0 +1,9 @@
+package com.atguigu.gmall.realtime.bean
+/**
+  * Author: Travis
+  * Desc:  商品Spu样例类
+  */
+case class SpuInfo(
+                    id:String ,
+                    spu_name:String)
+
